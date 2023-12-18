8cf3572fc2458473816fd08b27cb62bf71fda1ff
tpotempa <tpotempa@gmail.com>
2023-12-18 12:07:17 +0100

    Prophet.

--- prediction.py
+++ prediction.py
@@ -3,7 +3,8 @@
    pip install pandas
    pip install matplotlib
    pip install scikit-learn
-   pip install seaborn"""
+   pip install seaborn
+   pip install prophet"""
 
 __author__ = "Tomasz Potempa"
 __copyright__ = "Katedra Informatyki"
@@ -11,10 +12,12 @@ __version__ = "1.3.0"
 
 from data.mining.predictive import *
 from data.datasources import *
+from prophet import Prophet
 import pandas as pd
 import numpy as np
 import warnings
 import os
+
 
 
 """Zapytanie do analizy sprzedaży wg lat"""
@@ -25,6 +28,13 @@ analysis_sale_by_year_query = "SELECT rok::integer, miesiac::integer, COUNT(id_u
                               "GROUP BY rok, miesiac " \
                               "ORDER BY rok, miesiac"
 
+"""Zapytanie do analizy sprzedaży wg dni"""
+analysis_sale_by_date_query = "SELECT make_date(rok::integer, miesiac::integer, dzien::integer) AS data, SUM(cena)::integer AS wartosc " \
+                              "FROM poczta_olap.sprzedaz NATURAL JOIN poczta_olap.nadawca " \
+                              "INNER JOIN poczta_olap.czas ON czas.id_czasu = sprzedaz.id_czasu_nadania " \
+                              "WHERE rok BETWEEN 2017 AND 2019 " \
+                              "GROUP BY 1 " \
+                              "ORDER BY 1"
 
 
 def make_experiment_prediction(query):
@@ -54,6 +64,9 @@ def make_experiment_prediction(query):
     # Utworzenie i wyświetlenie początkowych danych ZBIÓR TESTOWEGO
     df_test = df[df.rok > 2016]
     print(df_test.head, os.linesep)
+
+    print("Rozmiar ZBIORU UCZĄCEGO: ", df_train.loc[:, y_feature].count()/df.loc[:, y_feature].count())
+    print("Rozmiar ZBIORU TESTOWEGO: ", df_test.loc[:, y_feature].count()/df.loc[:, y_feature].count(), os.linesep)
 
     # Wykres punktowy z oznaczeniem lat ZBIÓR UCZĄCY
     visualize(df=df_train, x=x_feature, y=y_feature, title=title + "\n{Zbiór UCZĄCY}", grouping="rok")
@@ -100,6 +113,51 @@ def make_experiment_prediction(query):
     visualize_plot_scatter(x_plot=x_test, y_plot=df_prediction["y_pred"], x_scatter=x_test, y_scatter=y_test,
                            title=chart_title, xlabel=xlabel, ylabel=ylabel)
 
+def make_experiment_forecast(query):
+    """Eksperyment prognozowania"""
+    warnings.filterwarnings("ignore")
+
+    rs = connect(query)
+
+    # Utworzenie ramki danych
+    df = pd.DataFrame(rs, columns=["ds", "y"])
+    print(df, os.linesep)
+
+    x_feature = "ds"
+    y_feature = "y"
+    xlabel = "Data"
+    ylabel = "Wartość usług"
+    title = "Sprzedaż usług"
+
+    # Wizualizacja danych
+    plt.figure(figsize=(15, 10))
+    plt.plot(df["ds"], df["y"])
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+
+    # Utworzenie modelu prognozowania z wykorzystaniem algorytmu PROPHET
+    model = Prophet()
+    model.fit(df)
+
+    # Utworzenie ramki danych dla okresu przyszłego tj. 1 roku
+    future = model.make_future_dataframe(periods=365)
+    future.tail()
+
+    # Prognozowanie dla okresu przyszłego
+    forecast = model.predict(future)
+    forecast[["ds", "yhat", "yhat_lower", "yhat_upper"]].tail()
+
+    # Wizualizacja prognozy
+    figure = model.plot(forecast)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+
+#    fig2 = m.plot_components(forecast)
+
+
 
 """Uruchamienie eksperymentów"""
-make_experiment_prediction(analysis_sale_by_year_query)
+#make_experiment_prediction(analysis_sale_by_year_query)
+make_experiment_forecast(analysis_sale_by_date_query)
